8205952a9cb0dd7b5a239503e1af49469b0ecd75
stefanedwards <sme@iysik.com>
2017-04-27 14:50:16 +0100

    Initial commit with reposition_legend.

--- R/legends.r
+++ R/legends.r
@@ -14,6 +14,7 @@ NULL
 #' @export
 #' @author Stack Overflow
 #' @import ggplot2
+#' @seealso \code{\link{grid_arrange_shared_legend}}
 #' @examples
 #' dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
 #' (d <- ggplot(dsamp, aes(carat, price)) +
@@ -51,6 +52,7 @@ g_legend<-function(a.gplot){
 #'   \url{https://github.com/tidyverse/ggplot2/wiki/Share-a-legend-between-two-ggplot2-graphs}
 #' @import ggplot2 gridExtra grid
 #' @export
+#' @seealso \code{\link{g_legend}}
 #' @examples
 #' dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
 #' p1 <- qplot(carat, price, data = dsamp, colour = clarity)
@@ -95,3 +97,97 @@ grid_arrange_shared_legend <- function(...,
 }
 
 
+
+#' Reposition a legend onto a panel
+#' 
+#' Some descriptives under title.
+#' 
+#' To modify the look of the legend, use themes 
+#' 
+#' @param aplot a ggplot2 or gtable object.
+#' @param position Where to place the legend in the panel. 
+#'                 Overrules \code{x}, \code{y}, and \code{just} arguments.
+#' @param legend The legend to place.
+#' @param panel Name of panel in gtable.
+#' @param x horisontal coordinate of legend, with 0 at left.
+#' @param y vertical coordiante of legend, with 0 at bottom.
+#' @param just 'Anchor point' of legend; it is this point of the legend that is 
+#'             placed at the \code{x} and \code{y} coordinates.
+#' @param plot Logical, when \code{TRUE} (default), draws plot with legend 
+#'             repositioned on a new page.
+#' @return 
+#'   gtable object, invisibly, with legend repositioned.
+#'   Can be drawn with \code{\link[grid]{grid.draw}}.
+#' @import ggplot2 grid gridExtra gtable
+#' @export
+#' @examples
+#' dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
+#' (d <- ggplot(dsamp, aes(carat, price)) +
+#'  geom_point(aes(colour = clarity)))
+#'
+#' reposition_legend(d + theme(legend.position='bottom'), 'bottom right')
+#' 
+#' # To change the orientation of the legend, use theme's descriptors.
+#' reposition_legend(d + theme(legend.position='bottom'), 'top left')
+#' 
+#' # Use odd specifications, here offset the legend with half its height from the bottom.
+#' reposition_legend(d + theme(legend.position='bottom'), x=0.3, y=0, just=c(0, -0.5))
+reposition_legend <- function(aplot, 
+                             position=NULL, 
+                             legend=NULL,
+                             panel='panel',
+                             x=NULL,
+                             y=NULL,
+                             just=NULL,
+                             plot=TRUE) {
+  
+  # Work out positioning
+  if (!is.null(position)) {
+    position <- match.arg(position, c('bottom right','bottom','bottom left','left','top left','top','top right','right','center'))
+    just <- switch(position,
+      'bottom right' = c(x=1, y=0),
+      'bottom' = c(x=0.5, y=0),
+      'bottom left' = c(x=0, y=0),
+      'left' = c(x=0, y=0.5),
+      'top left' = c(x=0, y=1),
+      'top' = c(x=0.5, y=1),
+      'top right' = c(x=1, y=1),
+      'right' = c(x=1, y=0.5),
+      'center' = c(x=0.5, y=0.5)
+    )
+    x = unit(just[1], 'npc')
+    y = unit(just[2], 'npc')
+  } 
+  if (is.null(x) & is.null(y) & is.null(just)) {
+    stop('Either supply `position` or `x`, `y`, and `just` arguments.')
+  }
+  
+  # Extract legends and gtable
+  if (is.null(legend) & inherits(aplot, 'ggplot'))
+    legend <- g_legend(aplot)
+  
+  if (is.null(legend) || inherits(legend, 'zeroGrob')) 
+    stop('No legend given in arguments, or could not extract legend from plot.')
+  
+  if (!inherits(aplot, 'gtable'))
+    aplot <- ggplot_gtable(ggplot_build(aplot + theme(legend.position='hidden')))
+  
+  legend$vp <- viewport(x=x, y=y, just=just,  width=sum(legend$widths), height=sum(legend$heights))
+  
+  pn <- which(aplot$layout$name == panel)
+  if (length(pn) == 0) stop('Could not find panel named `',panel,'`.')
+  if (inherits(aplot$grobs[[pn]], 'zeroGrob')) {
+    aplot$grobs[[pn]] <- legend
+  } else {
+    aplot$grobs[[pn]]$vp <- viewport(x=unit(0, 'npc'), y=unit(0, 'npc'), just=c(0,0), name=panel)
+    aplot$grobs[[pn]]$children <- gList(aplot$grobs[[pn]]$children, `guide-box`=legend)
+    aplot$grobs[[pn]]$childrenOrder <- append(aplot$grobs[[pn]]$childrenOrder, 'guide-box')
+  }
+  
+  if (plot) {
+    grid.newpage()
+    grid.draw(aplot)
+  }
+  
+  invisible(aplot)
+}
---
+++ vignettes/legends.R
@@ -0,0 +1,28 @@
+## ----setup,include=FALSE-------------------------------------------------
+library(knitr)
+
+knitr::opts_chunk$set(fig.height=4, fig.width=6)
+
+## ------------------------------------------------------------------------
+library(ggplot2)
+dsamp <- diamonds[sample(nrow(diamonds), 1000), ]
+(d <- ggplot(dsamp, aes(carat, price)) +
+  geom_point(aes(colour = clarity)))
+
+## ----reposition_legend---------------------------------------------------
+reposition_legend(d, 'top left')
+
+## ------------------------------------------------------------------------
+d2 <- d + facet_grid(.~cut)
+ggplot_gtable(ggplot_build(d2))
+
+## ----reposition_legend_facet1--------------------------------------------
+reposition_legend(d2, 'top left', panel = 'panel-3-1')
+
+## ----reposition_legend_facet2--------------------------------------------
+reposition_legend(d + facet_wrap(~cut, ncol=3), 'top left', panel='panel-3-2')
+
+## ----reposition_legend_facet3--------------------------------------------
+d3 <- d + facet_wrap(~cut, ncol=3) + scale_color_discrete(guide=guide_legend(ncol=3))
+reposition_legend(d3, 'center', panel='panel-3-2')
+
