625dcc3b1cff45ce02baf617eb8e536e40ca58f1
stefanedwards <sme@iysik.com>
2017-04-27 22:32:35 +0100

    facet_rep_wrap now works!

--- R/facet-rep-lab.r
+++ R/facet-rep-lab.r
@@ -5,9 +5,9 @@ NULL
 NULL
 
 #' Lay out panels in a grid with axes replicated across all panels.
-#' 
+#'
 #' \strong{Documentation is straight from \code{\link[ggplot2]{facet_grid}}.}
-#' Only addition is this function \emph{copies the axis across all panels}. 
+#' Only addition is this function \emph{copies the axis across all panels}.
 #' \code{repeat.tick.labels=FALSE} removes the labels (numbering) on an axis.
 #'
 #' \code{facet_grid} forms a matrix of panels defined by row and column
@@ -63,15 +63,15 @@ NULL
 facet_rep_grid <- function(facets, shrink=TRUE, repeat.tick.labels=FALSE, .debug=FALSE, ...) {
   f <- facet_grid(facets, shrink=shrink, ...)
   params <- append(f$params, list(repeat.tick.labels=repeat.tick.labels, .debug=.debug))
-  ggproto(NULL, FacetGridRepeatLabels, 
+  ggproto(NULL, FacetGridRepeatLabels,
           shrink=shrink,
           params=params)
 }
 
 #' Removes labels from axis grobs.
-#' 
+#'
 #' Called from FacetGridRepeatLabels.
-#' 
+#'
 #' @param axisgrob Grob with an axis.
 #' @keywords interal
 remove_labels_from_axis <- function(axisgrob) {
@@ -97,33 +97,32 @@ FacetGridRepeatLabels <- ggplot2::ggproto('FacetGridRepeatLabels', `_inherit`=gg
   draw_panels  = function(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params) {
     table <- FacetGrid$draw_panels(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params)
     if (params$.debug) { saveRDS(table, file='table.rds')}
-    
+
     # Add axes across all panels
     panels <- table$layout[grepl("^panel", table$layout$name), , drop = FALSE]
-    #cord <- do.call(rbind, strsplit(panels$name, split='-', fixed = TRUE))
-    panels$col <- as.integer(as.factor(panels$l))# as.integer(cord[,3])
-    panels$row <- as.integer(as.factor(panels$t))#as.integer(cord[,2])
-    
+    panels$col <- as.integer(as.factor(panels$l))
+    panels$row <- as.integer(as.factor(panels$t))
+
     axis_b <- table$grobs[grepl('axis-b-[[:digit:]]+', table$layout$name)]
     axis_l <- table$grobs[grepl('axis-l-[[:digit:]]+', table$layout$name)]
     axis_t <- table$grobs[grepl('axis-t-[[:digit:]]+', table$layout$name)]
     axis_r <- table$grobs[grepl('axis-r-[[:digit:]]+', table$layout$name)]
-    
+
     if (params$repeat.tick.labels == FALSE) {
       axis_b <- lapply(axis_b, remove_labels_from_axis)
       axis_l <- lapply(axis_l, remove_labels_from_axis)
       axis_t <- lapply(axis_t, remove_labels_from_axis)
       axis_r <- lapply(axis_r, remove_labels_from_axis)
     }
-    
+
     panel_range <- find_panel(table)
     panel_range[,c('col','row')] <- c(max(panels$col), max(panels$row))
-    
+
     l_axis_column_added <- logical(panel_range$col)
     r_axis_column_added <- logical(panel_range$col)
     t_axis_row_added <- logical(panel_range$row)
     b_axis_row_added <- logical(panel_range$row)
-    
+
     for (i in nrow(panels):1) {
       p <- panels[i,]
       # Bottom
@@ -154,9 +153,9 @@ FacetGridRepeatLabels <- ggplot2::ggproto('FacetGridRepeatLabels', `_inherit`=gg
           table <- gtable_add_rows(table, max_height(axis_t), coord$t-1)
           table <- gtable_add_grob(table, axis_t[[p$col]], t=coord$t, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-t-%d-%d', p$col, p$row))
         } else {
-          table <- gtable_add_grob(table, axis_t[[p$col]], t=coord$t-1, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-t-%d-%d', p$col, p$row))        
+          table <- gtable_add_grob(table, axis_t[[p$col]], t=coord$t-1, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-t-%d-%d', p$col, p$row))
         }
-        
+
       }
       # Right
       if (p$col != panel_range$col & !inherits(axis_l[[p$row]], 'zeroGrob')) { # panel is not right-most, (add colun), add axis
@@ -167,151 +166,10 @@ FacetGridRepeatLabels <- ggplot2::ggproto('FacetGridRepeatLabels', `_inherit`=gg
         }
         table <- gtable_add_grob(table, axis_r[[p$row]], t=coord$t, b=coord$b, l=coord$r+1, clip='off', name=sprintf('axis-r-%d-%d', p$row, p$col))
       }
-      
+
     }
-    
+
     table
   }
 )
 
-# facet_wrap ---------------
-NULL
-
-#' Wrap a 1d ribbon of panels into 2d
-#'
-#' \code{facet_wrap} wraps a 1d sequence of panels into 2d. This is generally
-#' a better use of screen space than \code{\link{facet_grid}} because most
-#' displays are roughly rectangular.
-#'
-#' @param facets Either a formula or character vector. Use either a
-#'   one sided formula, \code{~a + b}, or a character vector, \code{c("a", "b")}.
-#' @param nrow,ncol Number of rows and columns.
-#' @param scales should Scales be fixed (\code{"fixed"}, the default),
-#'   free (\code{"free"}), or free in one dimension (\code{"free_x"},
-#'   \code{"free_y"}).
-#' @param strip.position By default, the labels are displayed on the top of
-#'   the plot. Using \code{strip.position} it is possible to place the labels on
-#'   either of the four sides by setting \code{strip.position = c("top",
-#'   "bottom", "left", "right")}
-#' @param dir Direction: either "h" for horizontal, the default, or "v", for
-#'   vertical.
-#' @inheritParams facet_rep_grid
-#' @export
-facet_rep_wrap <- function(facets, shrink=TRUE, repeat.tick.labels=FALSE, .debug=FALSE, ...) {
-  f <- facet_wrap(facets, shrink=shrink, ...)
-  params <- append(f$params, list(repeat.tick.labels=repeat.tick.labels, .debug=.debug))
-  ggproto(NULL, FacetWrapRepeatLabels, 
-          shrink=shrink,
-          params=params)
-}
-
-#' @rdname ggplot2-ggproto
-#' @format NULL
-#' @usage NULL
-#' @export
-#' @importFrom ggplot2 FacetWrap ggproto
-#' @importFrom gtable gtable_add_cols gtable_add_grob gtable_add_rows
-FacetWrapRepeatLabels <- ggplot2::ggproto('FacetWrapRepeatLabels', `_inherit`=ggplot2::FacetWrap,
-  draw_panels = function(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params) {
-    table <- FacetWrap$draw_panels(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params)
-    if (params$.debug) { saveRDS(list(table=table, panels=panels, params=params), file='table.rds')}
-    
-    # Add axes across all panels
-    panels <- table$layout[grepl("^panel", table$layout$name), , drop = FALSE]
-    panels$col <- as.integer(as.factor(panels$l))# as.integer(cord[,3])
-    panels$row <- as.integer(as.factor(panels$t))#as.integer(cord[,2])
-    
-    # axes are placed on all four sides of **all** panels, but are in most cases zeroGrobs.
-    
-    axis_b <- table$grobs[grepl('axis-b-[[:digit:]]+', table$layout$name)]
-    #axis_l <- table$grobs[grepl('axis-l-[[:digit:]]+', table$layout$name)]
-    #axis_t <- table$grobs[grepl('axis-t-[[:digit:]]+', table$layout$name)]
-    #axis_r <- table$grobs[grepl('axis-r-[[:digit:]]+', table$layout$name)]
-    #
-    #if (params$repeat.tick.labels == FALSE) {
-    #  axis_b <- lapply(axis_b, remove_labels_from_axis)
-    #  axis_l <- lapply(axis_l, remove_labels_from_axis)
-    #  axis_t <- lapply(axis_t, remove_labels_from_axis)
-    #  axis_r <- lapply(axis_r, remove_labels_from_axis)
-    #}
-    
-    prev.is.copy <- FALSE
-    update.widths <- integer(max(panels$col))
-    for (i in 2:max(panels$col)) {
-      for (j in 1:max(panels$row)) {
-        #pk <- which(panels$col == i - 1 & panels$row == j)
-        pl <- which(panels$col == i & panels$row == j)
-        if (length(pl) == 0) next
-        if (inherits(table$grobs[[which(table$layout$name == panels$name[pl])]], 'zeroGrob')) next
-        #if (length(pk) == 0 | length(pl) == 0) next
-        
-        prev.grob <- table$grobs[[which(table$layout$name == sprintf('axis-l-%i-%i', j, i-1))]]
-        if (!prev.is.copy & params$repeat.tick.labels) {
-          prev.grob <- remove_labels_from_axis(prev.grob)
-          prev.is.copy <- TRUE
-        }
-        table$grobs[[which(table$layout$name == sprintf('axis-l-%i-%i', j, i))]] <- prev.grob
-      }
-    }
-    # update widths
-    
-    
-    # panel_range <- ggplot2:::find_panel(table)
-    # panel_range[,c('col','row')] <- c(max(panels$col), max(panels$row))
-    # 
-    # l_axis_column_added <- logical(panel_range$col)
-    # r_axis_column_added <- logical(panel_range$col)
-    # t_axis_row_added <- logical(panel_range$row)
-    # b_axis_row_added <- logical(panel_range$row)
-    
-    
-    
-    # for (i in nrow(panels):1) {
-    #   p <- panels[i,]
-    #   # Bottom
-    #   if (p$row != panel_range$row & !inherits(axis_b[[p$col]], 'zeroGrob')) { # panel not in bottom row, (add row), add axis
-    #     coord <- table$layout[table$layout$name == p$name, ]
-    #     if (b_axis_row_added[p$row] == FALSE) {
-    #       b_axis_row_added[p$row] <- TRUE
-    #       table <- gtable_add_rows(table, max_height(axis_b), coord$b)
-    #     }
-    #     table <- gtable_add_grob(table, axis_b[[p$col]], t=coord$b+1, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-b-%d-%d', p$col, p$row))
-    #   }
-    #   # Left
-    #   if (p$col > 1 & !inherits(axis_l[[p$row]], 'zeroGrob')) { # panel is not left-most column, (add column), add axis
-    #     coord <- table$layout[table$layout$name == p$name, ]
-    #     if (l_axis_column_added[p$col] == FALSE) {
-    #       l_axis_column_added[p$col] <- TRUE
-    #       table <- gtable_add_cols(table, max_width(axis_l), coord$l-1)
-    #       table <- gtable_add_grob(table, axis_l[[p$row]], t=coord$t, b=coord$b, l=coord$l, clip='off', name=sprintf('axis-l-%d-%d', p$row, p$col))
-    #     } else {
-    #       table <- gtable_add_grob(table, axis_l[[p$row]], t=coord$t, b=coord$b, l=coord$l-1, clip='off', name=sprintf('axis-l-%d-%d', p$row, p$col))
-    #     }
-    #   }
-    #   # Top
-    #   if (p$row > 1 & !inherits(axis_t[[p$col]], 'zeroGrob')) { # panel not in top row, (add row), add axis
-    #     coord <- table$layout[table$layout$name == p$name, ]
-    #     if (t_axis_row_added[p$row] == FALSE) {
-    #       t_axis_row_added[p$row] <- TRUE
-    #       table <- gtable_add_rows(table, max_height(axis_t), coord$t-1)
-    #       table <- gtable_add_grob(table, axis_t[[p$col]], t=coord$t, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-t-%d-%d', p$col, p$row))
-    #     } else {
-    #       table <- gtable_add_grob(table, axis_t[[p$col]], t=coord$t-1, l=coord$l, r=coord$r, clip='off', name=sprintf('axis-t-%d-%d', p$col, p$row))        
-    #     }
-    #     
-    #   }
-    #   # Right
-    #   if (p$col != panel_range$col & !inherits(axis_l[[p$row]], 'zeroGrob')) { # panel is not right-most, (add colun), add axis
-    #     coord <- table$layout[table$layout$name == p$name, ]
-    #     if (r_axis_column_added[p$col] == FALSE) {
-    #       r_axis_column_added[p$col] <- TRUE
-    #       table <- gtable_add_cols(table, max_width(axis_r), coord$r)
-    #     }
-    #     table <- gtable_add_grob(table, axis_r[[p$row]], t=coord$t, b=coord$b, l=coord$r+1, clip='off', name=sprintf('axis-r-%d-%d', p$row, p$col))
-    #   }
-    #   
-    # }
-    
-    table
-  }
-)                      
---
+++ R/facet-wrap.r
@@ -0,0 +1,205 @@
+#' @include ggplot2.r
+NULL
+
+#' Wrap a 1d ribbon of panels into 2d
+#'
+#' \code{facet_wrap} wraps a 1d sequence of panels into 2d. This is generally
+#' a better use of screen space than \code{\link{facet_grid}} because most
+#' displays are roughly rectangular.
+#'
+#' @param facets Either a formula or character vector. Use either a
+#'   one sided formula, \code{~a + b}, or a character vector, \code{c("a", "b")}.
+#' @param nrow,ncol Number of rows and columns.
+#' @param scales should Scales be fixed (\code{"fixed"}, the default),
+#'   free (\code{"free"}), or free in one dimension (\code{"free_x"},
+#'   \code{"free_y"}).
+#' @param strip.position By default, the labels are displayed on the top of
+#'   the plot. Using \code{strip.position} it is possible to place the labels on
+#'   either of the four sides by setting \code{strip.position = c("top",
+#'   "bottom", "left", "right")}
+#' @param dir Direction: either "h" for horizontal, the default, or "v", for
+#'   vertical.
+#' @inheritParams facet_rep_grid
+#' @importFrom ggplot2 facet_wrap
+#' @export
+#' @examples
+#' ggplot(mpg, aes(displ, hwy)) +
+#'   geom_point() +
+#'   facet_wrap(~class)
+facet_rep_wrap <- function(facets, shrink=TRUE, repeat.tick.labels=FALSE, .debug=FALSE, ...) {
+  f <- facet_wrap(facets, shrink=shrink, ...)
+  params <- append(f$params, list(repeat.tick.labels=repeat.tick.labels, .debug=.debug))
+  ggproto(NULL, FacetWrapRepeatLabels,
+          shrink=shrink,
+          params=params)
+}
+
+
+#' @rdname ggplot2-ggproto
+#' @format NULL
+#' @usage NULL
+#' @export
+#' @importFrom ggplot2 FacetWrap ggproto
+#' @importFrom gtable gtable_add_cols gtable_add_grob gtable_add_rows
+FacetWrapRepeatLabels <- ggplot2::ggproto('FacetWrapRepeatLabels',
+                                          `_inherit`=ggplot2::FacetWrap,
+  draw_panels = function(panels, layout, x_scales, y_scales, ranges, coord, data, theme, params) {
+    # If coord is non-cartesian and (x is free or y is free)
+    # then throw error
+    if ((!inherits(coord, "CoordCartesian")) && (params$free$x || params$free$y)) {
+      stop("ggplot2 does not currently support free scales with a non-cartesian coord", call. = FALSE)
+    }
+    if (inherits(coord, "CoordFlip")) {
+      if (params$free$x) {
+        layout$SCALE_X <- seq_len(nrow(layout))
+      } else {
+        layout$SCALE_X <- 1L
+      }
+      if (params$free$y) {
+        layout$SCALE_Y <- seq_len(nrow(layout))
+      } else {
+        layout$SCALE_Y <- 1L
+      }
+    }
+
+    ncol <- max(layout$COL)
+    nrow <- max(layout$ROW)
+    n <- nrow(layout)
+    panel_order <- order(layout$ROW, layout$COL)
+    layout <- layout[panel_order, ]
+    panels <- panels[panel_order]
+    panel_pos <- ggplot2:::convertInd(layout$ROW, layout$COL, nrow)
+
+    axes <- ggplot2:::render_axes(ranges, ranges, coord, theme, transpose = TRUE)
+
+    labels_df <- layout[names(params$facets)]
+    attr(labels_df, "facet") <- "wrap"
+    strips <- render_strips(
+      structure(labels_df, type = "rows"),
+      structure(labels_df, type = "cols"),
+      params$labeller, theme)
+
+    # If user hasn't set aspect ratio, and we have fixed scales, then
+    # ask the coordinate system if it wants to specify one
+    aspect_ratio <- theme$aspect.ratio
+    if (is.null(aspect_ratio) && !params$free$x && !params$free$y) {
+      aspect_ratio <- coord$aspect(ranges[[1]])
+    }
+
+    if (is.null(aspect_ratio)) {
+      aspect_ratio <- 1
+      respect <- FALSE
+    } else {
+      respect <- TRUE
+    }
+
+    empty_table <- matrix(list(zeroGrob()), nrow = nrow, ncol = ncol)
+    panel_table <- empty_table
+    panel_table[panel_pos] <- panels
+    empties <- apply(panel_table, c(1,2), function(x) ggplot2:::is.zero(x[[1]]))
+    panel_table <- gtable_matrix("layout", panel_table,
+     widths = unit(rep(1, ncol), "null"),
+     heights = unit(rep(aspect_ratio, nrow), "null"), respect = respect, clip = "on", z = matrix(1, ncol = ncol, nrow = nrow))
+    panel_table$layout$name <- paste0('panel-', rep(seq_len(ncol), nrow), '-', rep(seq_len(nrow), each = ncol))
+
+    panel_table <- gtable_add_col_space(panel_table,
+      theme$panel.spacing.x %||% theme$panel.spacing)
+    panel_table <- gtable_add_row_space(panel_table,
+      theme$panel.spacing.y %||% theme$panel.spacing)
+
+    # Add axes
+    axis_mat_x_top <- empty_table
+    axis_mat_x_top[panel_pos] <- axes$x$top[layout$SCALE_X]
+    axis_mat_x_bottom <- empty_table
+    axis_mat_x_bottom[panel_pos] <- axes$x$bottom[layout$SCALE_X]
+    axis_mat_y_left <- empty_table
+    axis_mat_y_left[panel_pos] <- axes$y$left[layout$SCALE_Y]
+    axis_mat_y_right <- empty_table
+    axis_mat_y_right[panel_pos] <- axes$y$right[layout$SCALE_Y]
+    #if (!params$free$x) {
+    #  axis_mat_x_top[-1,]<- list(zeroGrob())
+    #  axis_mat_x_bottom[-nrow,]<- list(zeroGrob())
+    #}
+    #if (!params$free$y) {
+    #  axis_mat_y_left[, -1] <- list(zeroGrob())
+    #  axis_mat_y_right[, -ncol] <- list(zeroGrob())
+    #}
+    if (!params$repeat.tick.labels) {
+      axis_mat_x_top[-1,] <- lapply(axis_mat_x_top[-1,], remove_labels_from_axis)
+      axis_mat_x_bottom[-nrow,] <- lapply(axis_mat_x_bottom[-nrow,], remove_labels_from_axis)
+      axis_mat_y_left[,-1] <- lapply(axis_mat_y_left[,-1], remove_labels_from_axis)
+      axis_mat_y_right[, -ncol] <- lapply(axis_mat_y_right[, -ncol], remove_labels_from_axis)
+    }
+    axis_height_top <- unit(apply(axis_mat_x_top, 1, max_height), "cm")
+    axis_height_bottom <- unit(apply(axis_mat_x_bottom, 1, max_height), "cm")
+    axis_width_left <- unit(apply(axis_mat_y_left, 2, max_width), "cm")
+    axis_width_right <- unit(apply(axis_mat_y_right, 2, max_width), "cm")
+    # Add back missing axes
+    if (any(empties)) {
+      first_row <- which(apply(empties, 1, any))[1] - 1
+      first_col <- which(apply(empties, 2, any))[1] - 1
+      row_panels <- which(layout$ROW == first_row & layout$COL > first_col)
+      row_pos <- ggplot2:::convertInd(layout$ROW[row_panels], layout$COL[row_panels], nrow)
+      row_axes <- axes$x$bottom[layout$SCALE_X[row_panels]]
+      col_panels <- which(layout$ROW > first_row & layout$COL == first_col)
+      col_pos <- ggplot2:::convertInd(layout$ROW[col_panels], layout$COL[col_panels], nrow)
+      col_axes <- axes$y$right[layout$SCALE_Y[col_panels]]
+      if (params$strip.position == "bottom" &&
+          theme$strip.placement != "inside" &&
+          any(!vapply(row_axes, is.zero, logical(length(row_axes))))) {
+        warning("Suppressing axis rendering when strip.position = 'bottom' and strip.placement == 'outside'", call. = FALSE)
+      } else {
+        axis_mat_x_bottom[row_pos] <- row_axes
+      }
+      if (params$strip.position == "right" &&
+          theme$strip.placement != "inside" &&
+          any(!vapply(col_axes, is.zero, logical(length(col_axes))))) {
+        warning("Suppressing axis rendering when strip.position = 'right' and strip.placement == 'outside'", call. = FALSE)
+      } else {
+        axis_mat_y_right[col_pos] <- col_axes
+      }
+    }
+    panel_table <- ggplot2:::weave_tables_row(panel_table, axis_mat_x_top, -1, axis_height_top, "axis-t", 3)
+    panel_table <- ggplot2:::weave_tables_row(panel_table, axis_mat_x_bottom, 0, axis_height_bottom, "axis-b", 3)
+    panel_table <- ggplot2:::weave_tables_col(panel_table, axis_mat_y_left, -1, axis_width_left, "axis-l", 3)
+    panel_table <- ggplot2:::weave_tables_col(panel_table, axis_mat_y_right, 0, axis_width_right, "axis-r", 3)
+
+    strip_padding <- convertUnit(theme$strip.switch.pad.wrap, "cm")
+    strip_name <- paste0("strip-", substr(params$strip.position, 1, 1))
+    strip_mat <- empty_table
+    strip_mat[panel_pos] <- unlist(unname(strips), recursive = FALSE)[[params$strip.position]]
+    if (params$strip.position %in% c("top", "bottom")) {
+      inside <- (theme$strip.placement.x %||% theme$strip.placement %||% "inside") == "inside"
+      if (params$strip.position == "top") {
+        placement <- if (inside) -1 else -2
+        strip_pad <- axis_height_top
+      } else {
+        placement <- if (inside) 0 else 1
+        strip_pad <- axis_height_bottom
+      }
+      strip_height <- unit(apply(strip_mat, 1, max_height), "cm")
+      panel_table <- ggplot2:::weave_tables_row(panel_table, strip_mat, placement, strip_height, strip_name, 2, "on")
+      if (!inside) {
+        strip_pad[unclass(strip_pad) != 0] <- strip_padding
+        panel_table <- ggplot2:::weave_tables_row(panel_table, row_shift = placement, row_height = strip_pad)
+      }
+    } else {
+      inside <- (theme$strip.placement.y %||% theme$strip.placement %||% "inside") == "inside"
+      if (params$strip.position == "left") {
+        placement <- if (inside) -1 else -2
+        strip_pad <- axis_width_left
+      } else {
+        placement <- if (inside) 0 else 1
+        strip_pad <- axis_width_right
+      }
+      strip_pad[unclass(strip_pad) != 0] <- strip_padding
+      strip_width <- unit(apply(strip_mat, 2, max_width), "cm")
+      panel_table <- ggplot2:::weave_tables_col(panel_table, strip_mat, placement, strip_width, strip_name, 2, "on")
+      if (!inside) {
+        strip_pad[unclass(strip_pad) != 0] <- strip_padding
+        panel_table <- ggplot2:::weave_tables_col(panel_table, col_shift = placement, col_width = strip_pad)
+      }
+    }
+    panel_table
+  }
+)
