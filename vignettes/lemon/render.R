6cf6b642c231a474894edfcdd9fc6010ca43b96e
stefanedwards <sme@iysik.com>
2017-05-19 16:33:56 +0100

    Kinda ready for submission.

--- vignettes/lemon/render.R
+++ vignettes/lemon/render.R
@@ -1,8 +1,8 @@
 library(render)
 
 
-my_render <- function(...) {
-  stat <- render('edwards2017.rmd')
+my_render <- function(render=FALSE) {
+  if (render) stat <- render('edwards2017.rmd')
   
   #correct tex
   
@@ -14,18 +14,29 @@ my_render <- function(...) {
   tex <- gsub('\\textbackslash{}end\\{Sinput\\}', '\\end{Sinput}', tex, fixed=TRUE)
   tex <- gsub('\\textbackslash{}begin\\{Soutput\\}', '\\begin{Soutput}', tex, fixed=TRUE)
   tex <- gsub('\\textbackslash{}end\\{Soutput\\}', '\\end{Soutput}', tex, fixed=TRUE)
+  tex <- gsub('\\begin{Schunk}', '', tex, fixed=TRUE)
+  tex <- gsub('\\end{Schunk}', '', tex, fixed=TRUE)
+  
+  tex <- gsub('\\textbackslash{}begin\\{figure\\}{[}h!{]}', '\\begin{figure}[h!]', tex, fixed=TRUE)
+  tex <- gsub('\\textbackslash{}begin\\{figure\\}{[}hb!{]}', '\\begin{figure}[hb!]', tex, fixed=TRUE)
+  tex <- gsub('\\textbackslash{}end\\{figure\\}', '\\end{figure}', tex, fixed=TRUE)
   
   tex <- gsub('\'\\{e\\}', '\\\'{e}', tex, fixed=TRUE)
   tex <- gsub('\\textpm,1', '\\textpm\\,1', tex, fixed=TRUE)
-
+  
   #m <- gregexpr('\\\\begin\\{Schunk\\}(.*?)\\\\end\\{Schunk\\}', tex)[[1]]
   #if (m[1] != -1) {
   #  
   #}
   writeLines(tex, 'edwards2017.tex')
   
-  system2('pdflatex','RJwrapper')
-  system2('bibtex','RJwrapper')
-  system2('pdflatex','RJwrapper')
-  system2('pdflatex','RJwrapper')
+  tools::texi2pdf('RJwrapper')
 }
+
+## Updates:
+# Abstract
+# change_width
+"
+The bracket functions accepts arguments to change the direction of the end `ticks',
+as well as the length of the bracket and the length of the end `ticks'.
+"
