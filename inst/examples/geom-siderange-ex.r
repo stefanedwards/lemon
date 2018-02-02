206f3c2ef57d88298d632d4b3f28446291194290
stefanedwards <sme@iysik.com>
2018-02-02 18:28:50 +0000

    Sideranges seems to works with facets. Needs documentation.

--- inst/examples/geom-siderange-ex.r
+++ inst/examples/geom-siderange-ex.r
@@ -8,3 +8,12 @@ df$y <- with(df, ifelse(y > 1 & a=='horse', 1, y))
 (p <- ggplot(df, aes(x=x, y=y, colour=a)) + geom_point())
 
 p + geom_siderange()
+
+# It also works with facets
+
+p <- ggplot(mpg, aes(displ, hwy)) +
+  geom_point() +
+  facet_wrap(~class, nrow = 4)
+
+p + geom_siderange()
+
