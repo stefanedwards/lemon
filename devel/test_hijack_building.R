68e433ccd16d184c46defebce019b87c542fd6c5
Stefan McKinnon Høj-Edwards <smhe@kamstrup.com>
2018-07-23 10:46:03 +0200

    Hijacking and overruling a single axis ticks works.

--- devel/test_hijack_building.R
+++ devel/test_hijack_building.R
@@ -77,3 +77,88 @@ yes <- ggproto('yes',
 #   assign('nothing', nothing_, envir=from)
 #   from
 # })
+
+
+
+
+# And now for something completly different -------
+
+
+prependClass <- function(object, new_class) {
+  if (!inherits(plot, new_class)) 
+    class(object) <- c(new_class, class(object))
+  object
+}
+
+
+as.ggplot2twist <- function(plot) {
+  if (inherits(plot, 'ggplot2twist')) 
+    return(plot)
+  plot <- prependClass(plot, 'ggplot2twist')
+  #if (!'twist' %in% names(plot))
+  #  plot$twist <- list()
+  plot
+}
+
+TwistedContinuousScale = ggproto('TwistedContinuousScale', ScaleContinuousPosition,
+  draw = function(...) { # something that returns a grob
+    grid::gList(grid::linesGrob())
+  }
+)
+
+censor <- scales::censor
+twisted_y_scale <- function (name = waiver(), breaks = waiver(), minor_breaks = waiver(), 
+                             labels = waiver(), limits = NULL, expand = waiver(), oob = censor, 
+                             na.value = NA_real_, trans = "identity", position = "right", 
+                             sec.axis = waiver()) 
+{
+  sc <- continuous_scale(c("y", "ymin", "ymax", "yend", "yintercept", 
+                           "ymin_final", "ymax_final", "lower", "middle", "upper"), 
+                         "position_c", identity, name = name, breaks = breaks, 
+                         minor_breaks = minor_breaks, labels = labels, limits = limits, 
+                         expand = expand, oob = oob, na.value = na.value, trans = trans, 
+                         guide = "none", position = position, super = TwistedContinuousScale)
+  if (!is.waive(sec.axis)) {
+    if (is.formula(sec.axis)) 
+      sec.axis <- sec_axis(sec.axis)
+    if (!is.sec_axis(sec.axis)) 
+      stop("Secondary axes must be specified using 'sec_axis()'")
+    sc$secondary.axis <- sec.axis
+  }
+  prependClass(sc, 'twistedscale')
+  sc
+}
+
+ggplot_add.twistedscale <- function(object, plot, object_name) {
+  plot$scales$add(object)
+  plot <- prependClass(plot, 'ggplot2twist')
+  plot
+}
+
+ggplot_build.ggplot2twist <- function(plot) {
+  g_built <- NextMethod() #ggplot2:::ggplot_build.ggplot(plot)
+  #cat('lalal\n')
+  prependClass(g_built, 'g_built_twist')
+}
+
+ggplot_gtable.g_built_twist <- function(data) {
+  # data: list(data = data, layout = layout, plot = plot)
+  save(data, file='gbuild.Rdata')
+  gtable <- NextMethod()
+  if (inherits(data$plot$scales$get_scales('y'), 'TwistedContinuousScale')) {
+    cat('Inheriting...\n')
+    twistedy <- data$plot$scales$get_scales('y')  ## returns a scale object (atleast without facets)
+    # data$layout$panel_scales_y ## returns list, could be more for facets
+    if (nrow(data$layout$layout) == 1) {
+      name <- switch(twistedy$position, left='axis-l', right='axis-r')
+      cat(name, '\n')
+      i <- which(gtable$layout$name == name)
+      gtable$grobs[i] <- twistedy$draw()
+    }
+  }
+  gtable
+}
+
+
+p <- ggplot(mtcars, aes(wt, y=mpg)) + geom_point()
+(pt <- p + twisted_y_scale(name=NULL, sec.axis=dup_axis(name='mpg')))
