9e24c94caf3b7773b5d020b0e0b2a7779dc17599
Stefan McKinnon Høj-Edwards <smhe@kamstrup.com>
2018-08-17 11:21:55 +0200

    Bug: Cannot estimate width of additional parsed titlegrobs.

--- inst/examples/axis-annotation-ex.r
+++ inst/examples/axis-annotation-ex.r
@@ -10,19 +10,11 @@ l
 l + annotated_y_axis("x^2 == .(y)", y=150, parsed=FALSE,  tick=FALSE) +
     annotated_y_axis("x^2 + bar(x) == .(y)", y=mean(mtcars$hp), parsed=TRUE, tick=TRUE)
 
-p + annotated_y_axis("bar(x) == .(y)", y = mean(mtcars$hp),  parsed=TRUE, tick=FALSE)
+l + annotated_y_axis("bar(x) == .(y)", y = mean(mtcars$hp),  parsed=TRUE, tick=FALSE) 
 # use double equal signs, or the output becomes '=(...)' for some reason.
 
 l + annotated_y_axis('this is midway', y=sum(range(mtcars$hp))/2, print_value = FALSE, side='left')
 
-
-l <- l + annotated_y_axis('nah', y=300,  tick=TRUE) +
-l + annotated_y_axis('leftsidey', y=300, side='left') 
-  annotated_y_axis("x^2 == .(y)", y=250, parsed=TRUE, vjust=1, tick=TRUE) +
-  annotated_y_axis("bar(x) ~ yadayda == .(y)", y=150, parsed=TRUE, col='green', hjust=0, x=1)
-(l <- p +   annotated_y_axis("x^2 == .(y)", y=250, parsed=TRUE, vjust=0.3, tick=TRUE) )
-(l <- p +   annotated_y_axis("X == .(y)", y=250, parsed=TRUE, tick=TRUE) )
-
-l + annotated_x_axis(x=25, side='bottom', label='nada')
-
-p + annotated_y_axis('leftsidey', y=300, side='left') 
+# work around if an axis only contains parsed expressions
+p + annotated_y_axis("bar(x) == .(y)", y = mean(mtcars$hp),  parsed=TRUE, tick=FALSE) +
+  annotated_y_axis("some long string", y=100, tick=FALSE, print_value=FALSE, colour=NA)
