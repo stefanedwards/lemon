d4a274f5a8a5fd4712df9636ee1843f1c42909ff
Stefan McKinnon Høj-Edwards <smhe@kamstrup.com>
2018-10-30 14:40:01 +0100

    Completed check of scale_symmetric.

--- R/scale-symmetric.r
+++ R/scale-symmetric.r
@@ -2,14 +2,7 @@
 
 # Upgrades a x or y continuous scale to a symmetric.
 set_symmetric_scale <- function(sc, mid) {
-  # if (is.na(mid)) {
-  #   if (!is.null(sc$limits)) {
-  #     mid = diff(sc$limits)/2
-  #   } else {
-  #     stop('`mid` cannot be NA if limits are missing.')
-  #   }
-  # }
-  # 
+
   sc$mid <- mid
   sc$train <- function(self, x) {
     if (length(x) == 0) 
@@ -20,7 +13,7 @@ set_symmetric_scale <- function(sc, mid) {
   sc$get_limits <- function(self) {
     l <- ggplot2::ggproto_parent(ScaleContinuousPosition, self)$get_limits()
     l2 <- 2*self$mid - rev(l)
-    str(c(l,l2))
+    #str(c(l,l2))
     
     c(min(l[1],l2[1]), max(l[2],l2[2]))
   }
@@ -31,12 +24,16 @@ set_symmetric_scale <- function(sc, mid) {
 #' Symmetrix position scale for continuous x and y
 #' 
 #' \code{scale_x_symmetric} and \code{scale_y_symmetric} are like the default
-#' scales for continuous x and y, but ensures that the resulting 
+#' scales for continuous x and y, but ensures that the resulting scale is 
+#' centered around \code{mid}. Does not work when setting limits on the scale.
 #' 
+#' @param mid Value to center the scale around.
+#' @param ... Values passed on to \code{\link[ggplot2]{scale_continuous}}.
 #' @rdname scale_symmetric
 #' @import ggplot2
 #' @export
-#' 
+#' @examples
+#' library(ggplot2)
 #' df <- expand.grid(a=c(-1,0,1), b=c(-1,0,1))
 #' rnorm2 <- function(x,y,n,sdx,sdy) {
 #'   if (missing(sdy)) 
@@ -45,30 +42,41 @@ set_symmetric_scale <- function(sc, mid) {
 #' }
 #' df <- mapply(rnorm2,df$a, df$b, MoreArgs=list(n=30,sdx=1),SIMPLIFY=FALSE)
 #' df <- do.call(rbind, df)
-#' ggplot(df, aes(x=x,y=y)) + geom_point() + facet_grid(a~b, scales='free_x')
+#' (p <- ggplot(df, aes(x=x,y=y)) + geom_point() + 
+#'   facet_grid(a~b, scales='free_x')
+#' )
+#' p + scale_x_symmetric(mid=0)
 scale_x_symmetric <- function(mid=0, ...) {
   set_symmetric_scale(ggplot2::scale_x_continuous(...), mid=mid)
 }
 
-#' @rdname lemon-ggproto
-#' @keywords internal
-#' @format NULL
-#' @usage NULL
+#' @rdname scale_symmetric
 #' @export
-#' @import ggplot2
-ScaleContinuousPositionSymmetric <- ggproto("ScaleContinuousPositionSymmetric", 
-                                            `_inherit`=ggplot2::ScaleContinuousPosition,
+#' @inheritParams scale_x_symmetric
+scale_y_symmetric <- function(mid=0, ...) {
+  set_symmetric_scale(ggplot2::scale_x_continuous(...), mid=mid)
+}
 
-  range = ggplot2:::continuous_range(),
-  na.value = NA_real_,
-  rescaler = rescale, # Used by diverging and n colour gradients x
-  oob = censor,
-  minor_breaks = waiver(),
-
-  is_discrete = function() FALSE,
-
-  train = function(self, x) {
-    if (length(x) == 0) return()
-    self$range$train(x)
-  }
-)
+#' #' @rdname lemon-ggproto
+#' #' @keywords internal
+#' #' @format NULL
+#' #' @usage NULL
+#' #' @export
+#' #' @import ggplot2
+#' #' @import scales
+#' ScaleContinuousPositionSymmetric <- ggproto("ScaleContinuousPositionSymmetric", 
+#'                                             `_inherit`=ggplot2::ScaleContinuousPosition,
+#' 
+#'   range = ggplot2:::continuous_range(),
+#'   na.value = NA_real_,
+#'   rescaler = scales::rescale, # Used by diverging and n colour gradients x
+#'   oob = scales::censor,
+#'   minor_breaks = waiver(),
+#' 
+#'   is_discrete = function() FALSE,
+#' 
+#'   train = function(self, x) {
+#'     if (length(x) == 0) return()
+#'     self$range$train(x)
+#'   }
+#' )
